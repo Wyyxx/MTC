82daf7ef4fdfd7311298ae67933f005beecbd9d7
Wyxx <a1121120071@utch.edu.mx>
2024-12-16 01:38:14 -0600

    comentado

--- lib/pantallas/landing.dart
+++ lib/pantallas/landing.dart
@@ -4,7 +4,7 @@ import 'package:signature/signature.dart'; // Importar paquete para firma
 import 'login.dart';
 import 'package:intl/intl.dart';
 import 'dart:io'; // Para manejar archivos (imágenes seleccionadas)
-import 'package:pdf/pdf.dart';
+//import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
